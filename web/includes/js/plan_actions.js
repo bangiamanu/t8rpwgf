0afd331ffddfa0744a3a18eed930b5d45acc14eb
bangiamanu <manu.bangia@gmail.com>
2012-06-18 15:06:43 +0100

    commit icons

---
+++ web/includes/js/plan_actions.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Called when the new plan button is pressed on toolbar
+ */
+function plan_actions_new_plan(){
+    
+}
+
+/**
+ * Called when the open_plan button is pressed on toolbar
+ */
+function plan_actions_open_plan(){
+    
+}
+/**
+ * Called when the save_plan button is pressed on toolbar
+ */
+function plan_actions_save_plan(){
+    
+}
+
+/**
+ * Called when the email_plan button is pressed on toolbar
+ */
+function plan_actions_email_plan(){
+    
+}
+
+/**
+ * Called when the print_plan button is pressed on toolbar
+ */
+function plan_actions_print_plan(){
+    print_api_printEvents();
+}
+
+/**
+ * Called when the share_plan_facebook button is pressed on toolbar
+ */
+function plan_actions_share_plan_facebook(){
+    
+}
+
+/**
+ * Called when the tweet_planbutton is pressed on toolbar
+ */
+function plan_actions_tweet_plan(){
+    
+}
